aa7a15200608260687a239c698baab5e882acc55
胡琤波 <657225485@qq.com>
2022-10-07 23:50:08 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# clone78
+## 简介
+
+网格集成展示
+1. 配置成功 直接适配不同的表数据
+2. 排序 筛选
+
+## 适用端
+
+**适用于`js``ts`项目**
+
+|端|支持|
+|---|---|
+|Web|√|
+|微信小程序|√|
+|iOS|√|
+
+## 依赖
+
+无
+
+```bash
+ 
+```
+
+## 属性
+
+## 方法
+
+## DEMO 
+
+```js
+...
+//const clone78 = require('../dist/index').default;
+import clone78 from "@www778878net/clone78"
+let a = {x:1,y:2};
+let b = clone78(a)
+...
+```
+
+## OTHER
+
+you can see test/test.js
---
+++ test/test.js
@@ -0,0 +1,44 @@
+﻿'use strict';
+const clone78 = require('../dist/index').default;
+const expect = require('chai').expect;
+
+describe("it's not object" , () => {
+    it('number ', () => {
+        let a = 1;
+        let b = clone78(a)
+        b += 3;
+        expect(b).to.not.equal(a);
+        //done(); //  
+    });
+    it('string ', () => {
+        let a = "test";
+        let b = clone78(a)
+        b += "2";
+        expect(b).to.not.equal(a);
+        //done(); //  
+    });
+});
+
+describe("it's  object", () => {
+    it('null ', () => {
+        let a = null;
+        let b = clone78(a)
+        b  = 3;
+        expect(b).to.not.equal(a);
+        //done(); // 
+    });
+    it('Array ', () => {
+        let a = [1,2,3];
+        let b = clone78(a)
+        b.push(4);
+        expect(b).to.not.equal(a);
+        //done(); //  
+    });
+    it('Object ', () => {
+        let a = {x:1,y:2};
+        let b = clone78(a)
+        b["x"]=3;
+        expect(b).to.not.equal(a);
+        //done(); //  
+    });
+});

67e2c52d0a283e512644cc8cbaa7cbe32ad891df
www778878net <657225485@qq.com>
2022-10-21 09:20:03 +0000

    云开发

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-﻿export default function clone78(obj) {
-	if (typeof obj != 'object')
-		return obj;
-	//  null,object,array  ='object'; 
-	if (obj === null) {// 
-		return null;
-	}
+﻿export default function clone78(obj:any) {
+    if (typeof obj != 'object')
+        return obj;
+    //  null,object,array  ='object'; 
+    if (obj === null) {// 
+        return null;
+    }
 
-	let o
-	//  
-	if (obj instanceof Array) {
-		o = [];
-		for (var i = 0, len = obj.length; i < len; i++) {
-			o.push(clone78(obj[i]));
-		}
-		return o
-	}
-	//   
-	o = {};
-	for (var j in obj) {
-		o[j] = clone78(obj[j]);
-	}
+    let o:any
+    //  
+    if (obj instanceof Array) {
+        o = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            o.push(clone78(obj[i]));
+        }
+        return o
+    }
+    //   
+    o = {};
+    for (var j in obj) {
+        o[j] = clone78(obj[j]);
+    }
 
-	return o;
+    return o;
 
 
 }
